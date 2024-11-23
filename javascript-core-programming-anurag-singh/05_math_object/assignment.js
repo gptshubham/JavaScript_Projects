e05015747d0543d4e8b5a517320e0a22ec4e4c14
gptshubham <shubhamca76@gmail.com>
2024-11-23 14:23:49 +0530

    explored properties and Methods of Math Module in JavaScript

---
+++ javascript-core-programming-anurag-singh/05_math_object/assignment.js
@@ -0,0 +1,5 @@
+// Generate a random integer between 25 and 50 in JavaScript?
+const min = 25;
+const max = 50;
+const value = Math.floor(Math.random() * (max - min + 1) + min);
+console.log(value);
