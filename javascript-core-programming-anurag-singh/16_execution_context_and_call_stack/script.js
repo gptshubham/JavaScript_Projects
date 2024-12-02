b3c7cf6284077ab674ae50d757470473ffbed9f5
gptshubham <shubhamca76@gmail.com>
2024-12-02 16:15:45 +0530

    explored execution context and call stack in js

---
+++ javascript-core-programming-anurag-singh/16_execution_context_and_call_stack/script.js
@@ -0,0 +1,37 @@
+// debugger
+/*
+// Execution Context
+sayHi()
+
+const username = 'Shubham'
+// console.log(username)
+
+const userAge = 15
+// console.log(userAge)
+
+function sayHi() {
+  const a = 14
+  const b = 12
+  console.log('Hi')
+  add(a, b)
+}
+
+function add(x, y) {
+  return x + y
+}
+
+console.log('Program Ended...')
+*/
+
+// Call Stack
+/*
+function introduceMe() {
+  console.log('Hi, My name is Shubham!')
+  introduceMe()
+}
+
+introduceMe()
+
+console.log('Program Ended...')
+// Note: this code keeps adding new introduceMe to the call stack every time the function calls itself (recursion) and it keeps going on indefinitely leading to stack overflow.
+*/
