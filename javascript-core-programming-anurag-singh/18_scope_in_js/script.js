e1c7cc747803104e9f4e5bd0d2a26c8dcfa3c530
gptshubham <shubhamca76@gmail.com>
2024-12-03 13:40:12 +0530

    explored lexical and block scope in js

--- javascript-core-programming-anurag-singh/18_scope_in_js/script.js
+++ javascript-core-programming-anurag-singh/18_scope_in_js/script.js
@@ -1,4 +1,7 @@
-debugger
+'use strict'
+// debugger
+
+// Global vs Local vs Lexical Scope
 
 const username = 'Shubham'
 let userAge = 22
@@ -13,16 +16,56 @@ function add() {
 }
 
 function addition() {
+  debugger
   const x = 15
   const y = 18
+  const z = 95
   console.log(x + y)
   console.log(username)
+
+  function added() {
+    const sum = 2 + 3
+    // block scope
+    {
+      const num1 = 78
+      console.log(num1)
+      var num2 = 98
+      console.log(num2)
+      // num3 = 120 // window scope (global) --> if not using strict mode.
+      // ReferenceError: num3 is not defined --> if using strict mode
+    }
+    // console.log(num1) // ReferenceError: num1 is not defined
+    console.log(num2)
+
+    console.log(sum)
+    console.log(z)
+    console.log(username)
+    console.log(a)
+    console.log(x)
+    console.log(y)
+
+    function toAdd() {
+      const total = 32 + 18
+      console.log(total)
+      console.log(sum)
+      console.log(x)
+      console.log(z)
+      console.log(username)
+    }
+
+    toAdd()
+  }
+
+  added()
+  // toAdd() // ReferenceError: toAdd is not defined
+  // console.log(num2) // ReferenceError: num2 is not defined
 }
 
 console.log(username)
-add()
+// add()
 addition()
-console.log(x) // ReferenceError: x is not defined
-console.log(y) // ReferenceError: y is not defined
-
+// console.log(x) // ReferenceError: x is not defined
+// console.log(y) // ReferenceError: y is not defined
+// console.log(num3) // window scope (global) --> if not using strict mode.
+// ReferenceError: num3 is not defined --> if using strict mode
 console.log('Program Ended...')
