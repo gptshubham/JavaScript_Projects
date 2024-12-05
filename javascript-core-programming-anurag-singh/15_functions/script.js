c2092db329224bc360a7c08405ac880df1ac96b3
gptshubham <shubhamca76@gmail.com>
2024-12-05 23:05:35 +0530

    explored methods in js

--- javascript-core-programming-anurag-singh/15_functions/script.js
+++ javascript-core-programming-anurag-singh/15_functions/script.js
@@ -23,33 +23,65 @@ function introduceMe(
   country = 'USA',
   profession = 'Software Engineer'
 ) {
-  console.log(`Hello!`)
-  console.log(`I'm ${name} from ${country}.`)
-  console.log(`I'm a ${profession}.`)
-  console.log('***************************')
+  console.log(`Hello!`);
+  console.log(`I'm ${name} from ${country}.`);
+  console.log(`I'm a ${profession}.`);
+  console.log('***************************');
 }
 
 // Function Call
-introduceMe('Shubham', 'India', 'Web Developer')
-introduceMe('Donald Trump', 'USA', 'President')
-introduceMe('Virat Kohli', 'India', 'Cricketer')
+introduceMe('Shubham', 'India', 'Web Developer');
+introduceMe('Donald Trump', 'USA', 'President');
+introduceMe('Virat Kohli', 'India', 'Cricketer');
 
-const returnValue = introduceMe()
-console.log(returnValue)
+const returnValue = introduceMe();
+console.log(returnValue);
 
 // return keyword in JS
 
 // Program: function that adds two numbers
 
 function addTwoNumbers(num1 = 0, num2 = 0) {
-  return num1 + num2
+  return num1 + num2;
 }
-let total = addTwoNumbers(10, 15)
-console.log(total)
-console.log(addTwoNumbers())
+let total = addTwoNumbers(10, 15);
+console.log(total);
+console.log(addTwoNumbers());
 
-total = addTwoNumbers(16 + 4, 12 + 18)
-console.log(total)
+total = addTwoNumbers(16 + 4, 12 + 18);
+console.log(total);
 
-total = addTwoNumbers(addTwoNumbers(4, 6), addTwoNumbers(8, 12))
-console.log(total)
+total = addTwoNumbers(addTwoNumbers(4, 6), addTwoNumbers(8, 12));
+console.log(total);
+
+// Functions vs Methods
+
+// Methods --> function defined within an object
+const maths = {
+  e: Math.E,
+  add: function (a, b) {
+    return a + b;
+  },
+  square: function (n) {
+    return n * n;
+  },
+  // latest way of defining a method in js
+  subtract(a, b) {
+    return a - b;
+  },
+  cube(n) {
+    return n ** 3;
+  },
+};
+
+let result = maths.add(2, 3);
+console.log(result);
+
+result = maths.square(5);
+console.log(result);
+
+result = maths.subtract(2, 4);
+console.log(result);
+
+result = maths.cube(2);
+console.log(result);
