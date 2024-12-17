c40cda37a08c418e8929c65d8c5371b6519296ba
gptshubham <shubhamca76@gmail.com>
2024-12-17 20:08:53 +0530

    explored the diff. among innerHTML, innerText and textContent

---
+++ javascript-core-programming-anurag-singh/3_dom_manipulation/32_innerText_vs_textContent/script.js
@@ -0,0 +1,14 @@
+const paragraph = document.querySelector('p');
+console.log(paragraph);
+
+console.log(paragraph.innerHTML);
+
+// paragraph.innerHTML = '<b>SKG</b>';
+
+console.log(paragraph.innerText);
+
+// paragraph.innerText = '<b>SKG</b>'
+
+console.log(paragraph.textContent);
+
+// paragraph.textContent = '<b>SKG</b>'
